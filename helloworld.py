29a1b1dceb54e8f274b75a4251edf89f2c845f44
Zeliang Wang <zeliangw@yahoo.com>
2018-02-09 01:00:53 +0000

    update

---
+++ helloworld.py
@@ -0,0 +1,6 @@
+Python 2.7.14 |Anaconda custom (64-bit)| (default, Oct  5 2017, 02:28:52) 
+[GCC 4.2.1 Compatible Clang 4.0.1 (tags/RELEASE_401/final)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> print "Hello World!"
+Hello World!
+>>> 
